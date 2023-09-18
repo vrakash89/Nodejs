6e82e64698131e1b5017f0d5931e8ae276df5d9d
vrakash89 <49906737+vrakash89@users.noreply.github.com>
2023-09-19 02:03:36 +0530

    Python script

---
+++ Asignment.py
@@ -0,0 +1,7 @@
+## New Asignment
+ten = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety","hundred"]
+for x in range(101):
+  if (x%10 == 0) :
+      print(ten[x//10])
+  else :
+    print(x)
