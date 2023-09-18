4c6b9b3df68069fe61899883f73292f3f1e4f7ec
vrakash89 <49906737+vrakash89@users.noreply.github.com>
2023-09-19 02:10:23 +0530

    Update Asignment.py

--- Asignment.py
+++ Asignment.py
@@ -1,4 +1,4 @@
-## New Asignment
+## New Assignment
 ten = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety","hundred"]
 for x in range(101):
   if (x%10 == 0) :
