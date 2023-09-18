ca89da0e0ef0abb64135e86b351a14448d61f635
vrakash89 <49906737+vrakash89@users.noreply.github.com>
2023-09-19 02:11:37 +0530

    Delete Asignment.py

--- Asignment.py
+++
@@ -1,7 +0,0 @@
-## New Assignment
-ten = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety","hundred"]
-for x in range(101):
-  if (x%10 == 0) :
-      print(ten[x//10])
-  else :
-    print(x)
